bb7c070b68ca6847ac0dcfbf02cbb8c404466b37
ARYANPATEL-BIT <aryan.patel7291@gmail.com>
2026-03-17 23:07:47 +0530

    Added a HTTpExection and PATH

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, HTTPException
 import json
 
 app = FastAPI()
@@ -23,3 +23,11 @@ def view():
 
     return data
 
+@app.get('/patient/{patient_id}')
+def view_patient(patient_id: str = Path(..., description='ID of the patient in the DB', example='P007')):
+    # load all the patients
+    data = load_data()
+
+    if patient_id in data:
+        return data[patient_id]
+    return HTTPException(status_code=404, detail='Patient not found')
