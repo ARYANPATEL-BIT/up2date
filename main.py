68e1115ebc65e2a7e9cd74ac230afa7c1d2915b7
ARYANPATEL-BIT <aryan.patel7291@gmail.com>
2026-03-18 19:47:24 +0530

    added pydantic

--- main.py
+++ main.py
@@ -1,7 +1,37 @@
 from fastapi import FastAPI, Path, HTTPException, Query
+from pydantic import BaseModel, Field, computed_field
+from typing import Annotated, Literal
 import json
 
 app = FastAPI()
+
+class Patient(BaseModel):
+
+    id: Annotated[str, Field(..., description='ID of the patient', examples=['P001'])]
+    name: Annotated[str, Field(..., description='Name of the Patient')]
+    city: Annotated[str, Field(..., description='City where the patient is living')]
+    age: Annotated[int, Field(..., gt=0, lt=120, description='Age of the patient')]
+    gender: Annotated[Literal['Male','Female','Others'],Field(..., description='Gender of the Patient')]
+    height: Annotated[float, Field(..., gt=0, description='Height of the patient in meters')]
+    weight: Annotated[float, Field(..., gt=0, description='Weight of the patient in kg')]
+
+    @computed_field
+    @property
+    def bmi(self) -> float:
+        bmi = round(self.weight/(self.height**2),2)
+        return bmi
+    
+    @computed_field
+    @property
+    def verdict(self) -> str:
+        if self.bmi < 18.5:
+            return 'Underweight'
+        elif self.bmi < 25:
+            return 'Normal'
+        elif self.bmi < 30:
+            return 'Normal'
+        else:
+            return 'Obese'
 
 def load_data():
     with open('patients.json','r') as f:
@@ -52,3 +82,4 @@ def sort_patients(sort_by: str = Query(..., description='Sort on the basis of he
     sorted_data = sorted(data.values(), key = lambda x: x.get(sort_by,0), reverse = sort_order)
 
     return sorted_data
+
