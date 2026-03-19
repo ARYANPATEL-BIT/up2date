9450a6a4628762187c5c8bfd25d894036ba2a653
ARYANPATEL-BIT <aryan.patel7291@gmail.com>
2026-03-19 12:15:57 +0530

    added put endpoint

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Path, HTTPException, Query
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel, Field, computed_field
-from typing import Annotated, Literal
+from typing import Annotated, Literal, Optional
 import json
 
 app = FastAPI()
@@ -33,6 +33,15 @@ class Patient(BaseModel):
             return 'Overweight'
         else:
             return 'Obese'
+        
+
+class PatientUpdate(BaseModel):
+    name: Annotated[Optional[str], Field(default=None)]
+    city: Annotated[Optional[str], Field(default=None)]
+    age: Annotated[Optional[int], Field(default=None, gt=0)]
+    gender: Annotated[Optional[Literal['Male','Female','Others']], Field(default=None)]
+    height: Annotated[Optional[float], Field(default=None, gt=0)]
+    weight: Annotated[Optional[float], Field(default=None, gt=0)]
 
 def load_data():
     with open('patients.json','r') as f:
@@ -105,3 +114,34 @@ def create_patient(patient: Patient):
     save_data(data)
 
     return JSONResponse(status_code=201, content={'Message':'Patient created successfully'})
+
+@app.put('/edit/{patient_id}')
+def update_patient(patient_id: str, patient_update: PatientUpdate):
+
+    data = load_data()
+
+    if patient_id not in data:
+        raise HTTPException(status_code=404, detail='Patient not found')
+    
+    existing_patient_info = data[patient_id].copy()
+    existing_patient_info['id'] = patient_id
+
+    updated_patient_info = patient_update.model_dump(exclude_unset=True, exclude_none=True)
+
+    existing_patient_info.update(updated_patient_info)
+
+    # for key, value in updated_patient_info.items():
+    #     existing_patient_info[key] = value
+
+    # existing_patient_info -> pydantic boject -> update bmi + verdict
+    # existing_patient_info['id'] = patient_id
+    patient_obj = Patient(**existing_patient_info)
+
+    # pydantic boject -> dict
+    data[patient_id] = patient_obj.model_dump(exclude={'id'})
+
+    # add this dict to data
+    # save data
+    save_data(data)
+
+    return JSONResponse(status_code=200, content={'message':'Patient updated'})
